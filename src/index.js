1d0508e14025ebd5b9956af0065dae1984e6b26f
Sonny Luu <sonnyluu@gmail.com>
2022-08-02 23:07:51 +0200

    add slash command mock

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import { config } from "dotenv";
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, Routes } from "discord.js";
+import { REST } from "@discordjs/rest";
 
 config();
+const TOKEN = process.env.BOT_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
 
 const client = new Client({
   intents: [
@@ -10,18 +14,30 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
   ],
 });
-const TOKEN = process.env.BOT_TOKEN;
 
-client.once("ready", () => {
-  console.log("Ready!");
-});
-
-client.login(TOKEN);
+const rest = new REST({ version: "10" }).setToken(TOKEN);
 
 client.on("ready", () => {
   console.log(`${client.user.username} is online!`);
 });
 
-client.on("messageCreate", (message) => {
-  console.log(`${message.author.tag}: ${message.content}`);
-});
+async function main() {
+  const commands = [
+    {
+      name: "tutorialHelp",
+      description: "Tutorial Help Description",
+    },
+  ];
+
+  try {
+    console.log("Started refreshing application (/) commands.");
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+      body: commands,
+    });
+    client.login(TOKEN);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
