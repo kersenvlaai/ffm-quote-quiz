d45832ae799b229302ea9693cccb7d7a27bccbb4
Sonny Luu <sonnyluu@gmail.com>
2022-08-12 20:43:15 +0200

    import * instead of single quotes

--- src/index.js
+++ src/index.js
@@ -2,19 +2,7 @@ import { config } from "dotenv";
 import { Client, GatewayIntentBits, Routes } from "discord.js";
 import { REST } from "@discordjs/rest";
 
-import {
-  sonny,
-  coen,
-  luuk,
-  marijn,
-  martijn,
-  lieke,
-  kara,
-  ying,
-  jeroen,
-  leander,
-  tom,
-} from "../constants/quotes.js";
+import * as quotes from "../constants/quotes.js";
 import { commands } from "../constants/commands.js";
 
 config();
@@ -49,86 +37,86 @@ client.on("interactionCreate", (interaction) => {
     switch (inputName) {
       case (inputName = "coen"):
         interaction.reply({
-          content: `"${coen[random(coen.length)]}" - ${capFirstLetter(
-            inputName
-          )}`,
+          content: `"${
+            quotes.coen[random(quotes.coen.length)]
+          }" - ${capFirstLetter(inputName)}`,
         });
         break;
       case (inputName = "ian"):
         interaction.reply({
-          content: `"${ian[random(ian.length)]}" - ${capFirstLetter(
-            inputName
-          )}`,
+          content: `"${
+            quotes.ian[random(quotes.ian.length)]
+          }" - ${capFirstLetter(inputName)}`,
         });
         break;
       case (inputName = "jeroen"):
         interaction.reply({
-          content: `"${jeroen[random(jeroen.length)]}" - ${capFirstLetter(
-            inputName
-          )}`,
+          content: `"${
+            quotes.jeroen[random(quotes.jeroen.length)]
+          }" - ${capFirstLetter(inputName)}`,
         });
         break;
       case (inputName = "kara"):
         interaction.reply({
-          content: `"${kara[random(kara.length)]}" - ${capFirstLetter(
-            inputName
-          )}`,
+          content: `"${
+            quotes.kara[random(kquotes.ara.length)]
+          }" - ${capFirstLetter(inputName)}`,
         });
         break;
       case (inputName = "leander"):
         interaction.reply({
-          content: `"${leander[random(leander.length)]}" - ${capFirstLetter(
-            inputName
-          )}`,
+          content: `"${
+            quotes.leander[random(quotes.leander.length)]
+          }" - ${capFirstLetter(inputName)}`,
         });
         break;
       case (inputName = "lieke"):
         interaction.reply({
-          content: `"${lieke[random(lieke.length)]}" - ${capFirstLetter(
-            inputName
-          )}`,
+          content: `"${
+            quotes.lieke[random(quotes.lieke.length)]
+          }" - ${capFirstLetter(inputName)}`,
         });
         break;
       case (inputName = "luuk"):
         interaction.reply({
-          content: `"${luuk[random(luuk.length)]}" - ${capFirstLetter(
-            inputName
-          )}`,
+          content: `"${
+            quotes.luuk[random(quotes.luuk.length)]
+          }" - ${capFirstLetter(inputName)}`,
         });
         break;
       case (inputName = "marijn"):
         interaction.reply({
-          content: `"${marijn[random(marijn.length)]}" - ${capFirstLetter(
-            inputName
-          )}`,
+          content: `"${
+            quotes.marijn[random(quotes.marijn.length)]
+          }" - ${capFirstLetter(inputName)}`,
         });
         break;
       case (inputName = "martijn"):
         interaction.reply({
-          content: `"${martijn[random(martijn.length)]}" - ${capFirstLetter(
-            inputName
-          )}`,
+          content: `"${
+            quotes.martijn[random(quotes.martijn.length)]
+          }" - ${capFirstLetter(inputName)}`,
         });
         break;
       case (inputName = "sonny"):
         interaction.reply({
-          content: `"${sonny[random(sonny.length)]}" - ${capFirstLetter(
-            inputName
-          )}`,
+          content: `"${
+            quotes.sonny[random(quotes.sonny.length)]
+          }" - ${capFirstLetter(inputName)}`,
         });
         break;
       case (inputName = "tom"):
         interaction.reply({
-          content: `"${tom[random(tom.length)]}" - ${capFirstLetter(
-            inputName
-          )}`,
+          content: `"${
+            quotes.tom[random(quotes.tom.length)]
+          }" - ${capFirstLetter(inputName)}`,
         });
         break;
       case (inputName = "ying"):
         interaction.reply({
-          content: `"${ying[random(ying.length)]}" - ${capFirstLetter(
-            inputName
-          )}`,
+          content: `"${
+            quotes.ying[random(quotes.ying.length)]
+          }" - ${capFirstLetter(inputName)}`,
         });
         break;
     }
