efe03883a03ac960b06c8547bacbee9f83903f17
Sonny Luu <sonnyluu@gmail.com>
2022-10-12 23:44:40 +0200

    use luxon instead of date-fns

--- helpers/dates.js
+++ helpers/dates.js
@@ -1,9 +1,9 @@
 import { differenceInHours } from "date-fns";
+import { DateTime } from "luxon";
 
-export const sonnyVerlossing = differenceInHours(
-  new Date(2023, 1, 1),
-  new Date()
-);
+export const sonnyVerlossing = DateTime.now().diff(DateTime.local(2023, 1, 1), [
+  "hours",
+]);
 
 export const martijnVerlossing = differenceInHours(
   new Date(2022, 9, 12),
