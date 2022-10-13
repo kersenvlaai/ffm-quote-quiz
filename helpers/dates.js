ec6b263bec2c1553616908c4fc11c6e91939632b
Sonny Luu <sonnyluu@gmail.com>
2022-10-13 08:35:36 +0200

    add hours and minutes

--- helpers/dates.js
+++ helpers/dates.js
@@ -2,16 +2,16 @@ import { differenceInHours } from "date-fns";
 import { DateTime } from "luxon";
 
 export const sonnyVerlossing = differenceInHours(
-  new Date(2023, 1, 1),
+  new Date(2023, 1, 1, 0, 0, 0, 0),
   new Date()
 );
 
 export const martijnVerlossing = differenceInHours(
-  new Date(2022, 9, 12),
+  new Date(2022, 12, 9, 0, 0, 0, 0),
   new Date()
 );
 
 export const marijnVerlossing = differenceInHours(
-  new Date(2023, 15, 8),
+  new Date(2023, 8, 15, 0, 0, 0, 0),
   new Date()
 );
