d562488bf0707ac4290f3915888b21349cbda119
Sonny Luu <sonnyluu@gmail.com>
2022-10-16 21:05:25 +0200

    update time

--- helpers/dates.js
+++ helpers/dates.js
@@ -1,17 +1,19 @@
 import { differenceInHours } from "date-fns";
 import { DateTime } from "luxon";
 
+const today = new Date();
+
 export const sonnyVerlossing = differenceInHours(
-  new Date(2023, 1, 1, 0, 0, 0, 0),
-  new Date()
+  new Date(2022, 12, 31, 0, 0),
+  today
 );
 
 export const martijnVerlossing = differenceInHours(
-  new Date(2022, 12, 9, 0, 0, 0, 0),
-  new Date()
+  new Date(2022, 12, 9, 0, 0),
+  today
 );
 
 export const marijnVerlossing = differenceInHours(
-  new Date(2023, 8, 15, 0, 0, 0, 0),
-  new Date()
+  new Date(2023, 8, 15, 0, 0),
+  today
 );
