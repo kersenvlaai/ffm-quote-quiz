12cb155f4da964470638110be6c53d8ef071c570
Sonny Luu <sonnyluu@gmail.com>
2022-10-16 21:28:50 +0200

    date.now fix?

--- helpers/dates.js
+++ helpers/dates.js
@@ -5,17 +5,17 @@ const today = subDays(new Date(), 31);
 
 export const sonnyVerlossing = differenceInHours(
   new Date(2022, 12, 31, 0, 0),
-  today
+  new Date(today)
 );
 
 export const martijnVerlossing = differenceInHours(
   new Date(2022, 12, 9, 0, 0),
-  today
+  new Date(today)
 );
 
 export const marijnVerlossing = differenceInHours(
   new Date(2023, 8, 15, 0, 0),
-  today
+  new Date(Date.now())
 );
 
 // export const result = differenceInHours(new Date());
