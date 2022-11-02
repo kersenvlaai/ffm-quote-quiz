6af3355f12c88b66182b9bd716f34d66c6844dab
Sonny Luu <sonnyluu@gmail.com>
2022-11-02 23:02:16 +0100

    yes

--- helpers/dates.js
+++ helpers/dates.js
@@ -1,7 +1,7 @@
 import { differenceInHours, addDays } from "date-fns";
 
 const startDate = addDays(new Date(), 31);
-const sonnyEndDate = new Date(2022, 10, 5, 17, 0);
+const sonnyEndDate = new Date(2022, 11, 5);
 const martijnEndDate = new Date(2022, 12, 9);
 const marijnEndDate = new Date(2023, 8, 15);
 
