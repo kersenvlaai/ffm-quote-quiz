70fba0575a46c4e30bf7899ef52ff91520e22e12
Sonny Luu <sonnyluu@gmail.com>
2022-11-15 11:09:10 +0100

    fix date

--- helpers/dates.js
+++ helpers/dates.js
@@ -2,7 +2,7 @@ import { differenceInHours, addDays } from "date-fns";
 
 const startDate = addDays(new Date(), 30);
 const sonnyEndDate = new Date(2022, 11, 5);
-const martijnEndDate = new Date(2022, 12, 9);
+const martijnEndDate = new Date(2022, 12, 2);
 const marijnEndDate = new Date(2023, 8, 15);
 
 export const sonnyVerlossing = differenceInHours(sonnyEndDate, startDate);
